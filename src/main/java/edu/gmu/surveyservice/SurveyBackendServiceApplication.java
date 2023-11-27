28e1a8ad4a822ac870f87f693c30d8ca7a8e6c18
sanjeev <sanjeev1996s@gmail.com>
2023-11-26 23:27:46 -0500

    Intial Commit

---
+++ src/main/java/edu/gmu/surveyservice/SurveyBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.gmu.surveyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyBackendServiceApplication.class, args);
+	}
+
+}
